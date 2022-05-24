17f8aaab3e74bc3cff117b7e7814a63a1e0bb5cf
Zairu_ <loboser.sl@gmail.com>
2022-05-24 15:34:28 -0300

    inicio ejercicio1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace tp06
+{
+    public class Program {
+         static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Calculadora{
+        private double resultado;
+        
+        public Calculadora(){
+            
+        }
+    }
+}
